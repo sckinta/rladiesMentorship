a90a22e499800896453d5fd1ba218e965e0f15d4
sckinta <sckinta@gmail.com>
2020-03-07 07:11:10 -0500

    switch to googledrive, remove photo and add email

--- app.R
+++ app.R
@@ -1,112 +1,71 @@
-# remote::install_github("sckinta/aws.s3")
+# https://sckinta.shinyapps.io/mentorShiny/
 library(shinyWidgets)
 library(shinyjs)
 library(shiny)
 library(shinythemes)
 library(tidyverse)
-library(aws.s3)
+library(googledrive)
+library(googlesheets4)
 
-`%then%` <- shiny:::`%OR%`
-################### aws.s3 setting #####################
-s3BucketName <- "rladiesmentor"
-Sys.setenv("AWS_ACCESS_KEY_ID" = "[SECRET]",
-           "AWS_SECRET_ACCESS_KEY" = "[SECRET]",
-           "AWS_DEFAULT_REGION" = "us-east-2")
+# # designate project-specific cache
+options(
+        gargle_oauth_cache = ".secret",
+        gargle_oauth_email = TRUE
+)
 
-################ aws.s3 save and load data function ####################
+################ googlesheet save and load data function ####################
 saveData <- function(input) {
+        # download previous spreadsheet to tempfile
+        tmpDir=file.path(tempdir(),"mentors.csv")
+        mentors=drive_find(pattern = "mentors", type = "spreadsheet")
+        drive_download(as_id(mentors), type="csv", path=tmpDir, overwrite=T)
+        # drive_download("mentors", type="csv", path=tmpDir, overwrite=T)
+        
+        # read spreadsheet to df
+        df = read_csv(tmpDir)
+        
+        # read input to data
         data <- data.frame(matrix(nrow=1,ncol=0))
         for (x in fields) {
                 var <- input[[x]]
-                if (x == "photo_wig" & length(var)!=0){
-                        img_file=var$datapath
-                        if (grepl("\\.jpeg|\\.JPEG",img_file)){
-                                img_format=".jpeg"
-                        }
-                        if (grepl("\\.png|\\.PNG",img_file)){
-                                img_format=".png"
-                        }
-                }else if (x == "photo_wig" & length(var)==0){
-                        img_file="unknown.jpg"
+                if (length(var)==0){
+                        data[[x]] <- " "
                 }
-                else{
-                        if (length(var)==0){
-                                data[[x]] <- " "
-                        }
-                        else if (length(var) > 1 ) {
-                                # handles lists from checkboxGroup and multiple Select
-                                data[[x]] <- list(var)
-                        } else {
-                                # all other data types
-                                data[[x]] <- var
-                        }
+                else if (length(var) > 1 ) {
+                        # handles lists from checkboxGroup and multiple Select
+                        data[[x]] <- paste(var,collapse = ", ")
+                } else {
+                        # all other data types
+                        data[[x]] <- var
                 }
         }
-        data$submit_time <- date()
-        # Create a unique file name
-        name1=as.integer(Sys.time())
-        name2=digest::digest(data)
-        fileName <- sprintf(
-                "%s_%s.rds", 
-                name1, 
-                name2
+        
+        data$submit_time <- Sys.time()
+        colnames(data) = c("name","pronoun","linkedin", "email","signUp.type","expertises","primary.employment","preferred.mentor.method","submit.timestamp")
+        # append new data
+        df = bind_rows(df, data)
+        # write into tempfile
+        write_csv(df, path=tmpDir, na=" ")
+        # update mentors spreadsheet
+        mentors <- mentors %>% drive_update(
+                tmpDir,
+                name="mentors"
         )
-        # rename imagefilename
-        if (img_file!="unknown.jpg"){
-                img_newName <-sprintf(
-                        paste0("%s_%s",img_format), 
-                        name1, 
-                        name2
-                )
-                file.rename(from=img_file, to=file.path(tempdir(),img_newName))
-                # Upload the file to S3
-                put_object(file = file.path(tempdir(),img_newName), object = img_newName, bucket = s3BucketName, check_region = F, acl = "public-read")
-        }else{
-                img_newName = "unknown.jpg"
-        }
-        
-        # save tmp file here
-        
-        # Write the file to the local system
-        file_path <- file.path(tempdir(), fileName)
-        # saveRDS(
-        #         object = data,
-        #         file = file_path
-        # )
-        data["photo_wig"]=paste0("https://rladiesmentor.s3.us-east-2.amazonaws.com/",img_newName)
-        s3save(data, bucket = s3BucketName, object = fileName)
-        
-        
+        # drive_rm(mentors)
 }
 
 loadData <- function() {
-        # read all the files into a list
-        # files <- list.files(outputDir, full.names = TRUE, pattern="\\.rds")
-        files <- sapply(get_bucket(s3BucketName), function(x){x[["Key"]]})
-        files <- files[grepl("\\.rds",files)]
-        if (length(files) == 0) {
-                # create empty data frame with correct columns
-                field_list <- c(fields, "submit_time")
-                data <- data.frame(matrix(ncol = length(field_list), nrow = 0))
-                names(data) <- field_list
-        } else {
-                data <- lapply(files, function(x) {
-                        # object <- get_object(x, s3BucketName)
-                        # readRDS(object)
-                        s3load(x, bucket = s3BucketName)
-                        data
-                })
-                
-                # Concatenate all data together into one data.frame
-                data <- do.call(rbind, data)
-        }
-        colnames(data) = c("name","pronoun","linkedin", "signUp.type","expertises","primary.employment","preferred.mentor.method","submit.timestamp","photo.link")
+        # read spreadsheet
+        sheet_id=drive_find(pattern = "mentors", type = "spreadsheet")$id
+        data=read_sheet(sheet_id)
         # data
         out = tibble(
-                photo=sapply(data$photo.link,function(pic){paste0('<img src=',pic,' height=52></img>')})
-        )
-        out = out %>%
-                mutate(name=mapply(function(url,text){paste0("<a href='",url,"'>",text,"</a>")}, data$linkedin, data$name))
+                name=mapply(
+                        function(url,text){paste0("<a href='",url,"'>",text,"</a>")}, 
+                        data$linkedin, data$name
+                        ),
+                email=sapply(data$email,function(email){paste0("<a href='mailto:",email,"'>","email","</a>")})
+                )
         out = bind_cols(
                 out %>% as.data.frame(),
                 data[,c("pronoun","signUp.type","expertises","primary.employment","preferred.mentor.method")]
@@ -124,7 +83,7 @@ meets=c("In-person","Remote (e.g. by phone or online)")
 genders=c("She/her", "He/him", "They/them","Other")
 
 #################### define wigget (variable_name/field_name and inputId must be the same) #################
-fields <- c("name_wig", "gender_wig", "linkedin_wig", "photo_wig",
+fields <- c("name_wig", "gender_wig", "linkedin_wig","email_wig",
             "type_wig", "expertise_wig", "employment_wig", "meet_wig")
 
 name_wig <- textInput("name_wig", "Name:", "")
@@ -136,7 +95,8 @@ gender_wig  <- radioButtons(
         selected = "none"
 )
 linkedin_wig <- textInput("linkedin_wig","LinkedIn Profile Link:","")
-photo_wig <- fileInput("photo_wig", "Your photo (eg. .jpeg, .png)", accept = c("jpeg","png"))
+email_wig <- textInput("email_wig","Email:","")
+
 type_wig <- checkboxGroupInput(
         "type_wig",
         "Available as mentor and/or speaker?", 
@@ -170,6 +130,7 @@ resetForm <- function(session) {
         updateTextInput(session, "name_wig", value = "")
         updateRadioButtons(session, "gender_wig", selected = "none")
         updateTextInput(session, "linkedin_wig", value = "")
+        updateTextInput(session, "email_wig", value = "")
         updateCheckboxGroupInput(session, "type_wig", selected=character(0))
         updateSelectizeInput(session, "expertise_wig", selected=character(0))
         updateSelectizeInput(session, "employment_wig", selected=character(0))
@@ -208,7 +169,7 @@ ui <- navbarPage(
                 ),
                 fluidRow(
                         column(12,
-                               photo_wig,
+                               email_wig,
                                offset=3
                         )
                 ),
@@ -320,6 +281,12 @@ server <- function(input, output, session) {
                 )
                 removeNotification("linkedin_error", session = getDefaultReactiveDomain())
                 validate(
+                        need(grepl("\\@",input$email_wig), 
+                             showNotification("Please add a valid email", duration = 0, type = "error", id="email_error")
+                        )
+                )
+                removeNotification("email_error", session = getDefaultReactiveDomain())
+                validate(
                         need(input$type_wig!="", 
                              showNotification("Please select mentor or speaker", duration = 0, type = "error", id="type_error")
                         )
@@ -343,20 +310,21 @@ server <- function(input, output, session) {
         # },escape = FALSE)
         # 
         # #### search tab
-        v <- reactiveValues(data = loadData())
+        df = loadData()
+        v <- reactiveValues(data = df)
         
         observeEvent(input$search_submit, {
-                search_type_bl = unlist(lapply(loadData()$signUp.type, function(x){any(x %in% input$search_type)}))
-                search_gender_bl = unlist(lapply(loadData()$pronoun, function(x){any(x %in% input$search_gender)}))
-                search_meet_bl = unlist(lapply(loadData()$preferred.mentor.method, function(x){any(x %in% input$search_meet)}))
-                search_employment_bl = unlist(lapply(loadData()$primary.employment, function(x){any(x %in% input$search_employment)}))
-                search_expertise_bl = unlist(lapply(loadData()$expertises, function(x){any(x %in% input$search_expertise)}))
+                search_type_bl = unlist(lapply(df$signUp.type, function(x){any(strsplit(x,", ")[[1]] %in% input$search_type)}))
+                search_gender_bl = unlist(lapply(df$pronoun, function(x){any(strsplit(x,", ")[[1]]  %in% input$search_gender)}))
+                search_meet_bl = unlist(lapply(df$preferred.mentor.method, function(x){any(strsplit(x,", ")[[1]]  %in% input$search_meet)}))
+                search_employment_bl = unlist(lapply(df$primary.employment, function(x){any(strsplit(x,", ")[[1]]  %in% input$search_employment)}))
+                search_expertise_bl = unlist(lapply(df$expertises, function(x){any(strsplit(x,", ")[[1]]  %in% input$search_expertise)}))
                 bl = as.logical(search_type_bl*search_gender_bl*search_meet_bl*search_employment_bl*search_expertise_bl)
-                v$data = loadData()[bl,]
+                v$data = df[bl,]
         })
         
         output$responses <- renderDataTable({
-                # update with current response when Submit or Delete are clicked
+                # update with current response when Submit
                 input$submit
                 v$data
         },escape = FALSE)
